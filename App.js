af3d976cc888ad6be387e4907a502369d88a5321
kurohage <517326+kurohage@users.noreply.github.com>
2019-11-28 19:54:38 +0300

    auth login and register works. ugly, but works.

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+import { Spinner } from "native-base";
 import { StyleSheet } from "react-native";
 import AppContainer from "./navigation";
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    loading: true
+  };
+
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <Spinner color="white" />;
+    }
     return <AppContainer />;
   }
 }
