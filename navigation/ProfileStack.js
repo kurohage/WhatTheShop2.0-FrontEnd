f9dbd3818cdb84fd6757f12e3ed6744b9674311f
kurohage <517326+kurohage@users.noreply.github.com>
2019-12-08 21:04:07 +0300

    Added order details page to stack

--- navigation/ProfileStack.js
+++ navigation/ProfileStack.js
@@ -2,12 +2,14 @@ import { createStackNavigator } from "react-navigation-stack";
 
 // Components
 import ProfileScreen from "../components/Profile";
+import OrderDetailScreen from "../components/OrderDetail";
 import LoginScreen from "../components/Login";
 
 const ProfileStack = createStackNavigator(
   {
     Profile: ProfileScreen,
-    Login: LoginScreen
+    Login: LoginScreen,
+    OrderDetail: OrderDetailScreen
   },
   {
     initialRouteName: "Profile",
